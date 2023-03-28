e4ede87d9d8ed9722262491bab0bc70196db1225
dupy07 <129101293+dupy07@users.noreply.github.com>
2023-03-28 22:57:22 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+bar = document.getElementById("bar");
+close = document.getElementById("close");
+nav = document.getElementById("navbar");
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
