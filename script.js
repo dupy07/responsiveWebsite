90633c0998c571d61f583620bf244354a8e6e237
dupy07 <129101293+dupy07@users.noreply.github.com>
2023-04-16 12:15:51 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -15,52 +15,61 @@ if (close) {
 
 /*SELF JAVA*/
 var pro = document.getElementsByClassName("pro");
-pro[0].onclick = function () {
+
+pro[0].addEventListener("click", function () {
   window.location.href = "sproduct.html";
-};
+});
 
 pro[1].addEventListener("click", function () {
   window.location.href = "sproduct1.html";
 });
-pro[2].onclick = function () {
+pro[2].addEventListener("click", function () {
   window.location.href = "sproduct2.html";
-};
-pro[3].onclick = function () {
+});
+
+pro[3].addEventListener("click", function () {
   window.location.href = "sproduct3.html";
-};
-pro[4].onclick = function () {
+});
+pro[4].addEventListener("click", function () {
   window.location.href = "sproduct4.html";
-};
-pro[5].onclick = function () {
+});
+
+pro[5].addEventListener("click", function () {
   window.location.href = "sproduct5.html";
-};
-pro[6].onclick = function () {
+});
+pro[6].addEventListener("click", function () {
   window.location.href = "sproduct6.html";
-};
-pro[7].onclick = function () {
+});
+
+pro[7].addEventListener("click", function () {
   window.location.href = "sproduct7.html";
-};
-pro[8].onclick = function () {
+});
+pro[8].addEventListener("click", function () {
   window.location.href = "sproduct8.html";
-};
-pro[9].onclick = function () {
+});
+
+pro[9].addEventListener("click", function () {
   window.location.href = "sproduct9.html";
-};
-pro[10].onclick = function () {
+});
+pro[10].addEventListener("click", function () {
   window.location.href = "sproduct10.html";
-};
-pro[11].onclick = function () {
+});
+
+pro[11].addEventListener("click", function () {
   window.location.href = "sproduct11.html";
-};
-pro[12].onclick = function () {
+});
+pro[12].addEventListener("click", function () {
   window.location.href = "sproduct12.html";
-};
-pro[13].onclick = function () {
+});
+
+pro[13].addEventListener("click", function () {
   window.location.href = "sproduct13.html";
-};
-pro[14].onclick = function () {
+});
+
+pro[14].addEventListener("click", function () {
   window.location.href = "sproduct14.html";
-};
-pro[15].onclick = function () {
+});
+
+pro[15].addEventListener("click", function () {
   window.location.href = "sproduct15.html";
-};
+});
